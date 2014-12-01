0c8df3799d037b55cafc56f3d4c9619008f2892e
evply <evply@sina.com>
2014-12-01 09:54:07 +0800

    using liberator to rest service

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.2.0"]
                  [ring/ring-defaults "0.1.2"]
+                 [ring/ring-json "0.3.1"]
+                 [liberator "0.12.2"]
                  [org.immutant/web "2.0.0-beta1"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler hydrogen.core.handler/app}
--- src/hydrogen/core/handler.clj
+++ src/hydrogen/core/handler.clj
@@ -1,11 +1,18 @@
 (ns hydrogen.core.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [compojure.handler :as handler]
+            [liberator.core :refer [defresource]]
+            [ring.middleware.json :as middleware]))
+
+(defresource service [body]
+  :available-media-types ["application/json"]
+  :handle-ok {:a (str body)})
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/service/*" [body] service)
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> (handler/api app-routes)
+      (middleware/wrap-json-response)))
